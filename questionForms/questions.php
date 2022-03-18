75f93b33c6a9071b5756f374a746468eb035b787
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-03-17 21:55:34 -0700

    Deleted extra question files

--- questionForms/questions.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Questions Form 1</title>
-    </head>
-    <body>
-        <h1>Questions Form (Part 1)</h1>
-    </body>
-</html>
---
+++ questions.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Questions Form 1</title>
+    </head>
+    <body>
+        <h1>Questions Form (Part 1)</h1>
+        <div>
+            <h3>PHP was a difficult language to learn</h3>
+            <input type="radio" name="difficulty"/>
+            <label>Strongly Disagree</label>
+            <input type="radio" name="difficulty"/>
+            <label>Disagree</label>
+            <input type="radio" name="difficulty"/>
+            <label>Neutral</label>
+            <input type="radio" name="difficulty"/>
+            <label>Agree</label>
+            <input type="radio" name="difficulty"/>
+            <label>Strongly Agree</label>
+        </div>
+
+        <h3>I feel like I learned a lot from the experience</h3>
+
+        <h3>I was able to adapt quickly to the language</h3>
+
+        <h3>I am grateful that I took the time to learn PHP</h3>
+
+    </body>
+</html>

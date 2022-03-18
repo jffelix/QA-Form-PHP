04a3aa6b0e754f2f6a23453e31df847a841b71db
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-03-17 22:03:18 -0700

    Added radio inputs

--- questions.php
+++ questions.php
@@ -1,29 +1,64 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Questions Form 1</title>
+        <title>Survey Questions</title>
     </head>
     <body>
-        <h1>Questions Form (Part 1)</h1>
+        <h1>Survey Questions</h1>
+        <p>Read the following statements and select the choice that applies the most to you</p>
+        <br>
         <div>
             <h3>PHP was a difficult language to learn</h3>
-            <input type="radio" name="difficulty"/>
+            <input type="radio" id="stronglyDisagree" name="difficulty"/>
             <label>Strongly Disagree</label>
-            <input type="radio" name="difficulty"/>
+            <input type="radio" id="disagree"  name="difficulty"/>
             <label>Disagree</label>
-            <input type="radio" name="difficulty"/>
+            <input type="radio" id="neutral" name="difficulty"/>
             <label>Neutral</label>
-            <input type="radio" name="difficulty"/>
+            <input type="radio" id="agree" name="difficulty"/>
             <label>Agree</label>
-            <input type="radio" name="difficulty"/>
+            <input type="radio" id="stronglyAgree" name="difficulty"/>
             <label>Strongly Agree</label>
         </div>
-
-        <h3>I feel like I learned a lot from the experience</h3>
-
-        <h3>I was able to adapt quickly to the language</h3>
-
-        <h3>I am grateful that I took the time to learn PHP</h3>
+        <div>
+            <h3>I feel like I learned a lot from the experience</h3>
+            <input type="radio" id="stronglyDisagree" name="learning"/>
+            <label>Strongly Disagree</label>
+            <input type="radio" id="disagree"  name="learning"/>
+            <label>Disagree</label>
+            <input type="radio" id="neutral" name="learning"/>
+            <label>Neutral</label>
+            <input type="radio" id="agree" name="learning"/>
+            <label>Agree</label>
+            <input type="radio" id="stronglyAgree" name="learning"/>
+            <label>Strongly Agree</label>
+        </div>
+        <div>
+            <h3>I was able to adapt quickly to the language</h3>
+            <input type="radio" id="stronglyDisagree" name="adapt"/>
+            <label>Strongly Disagree</label>
+            <input type="radio" id="disagree"  name="adapt"/>
+            <label>Disagree</label>
+            <input type="radio" id="neutral" name="adapt"/>
+            <label>Neutral</label>
+            <input type="radio" id="agree" name="adapt"/>
+            <label>Agree</label>
+            <input type="radio" id="stronglyAgree" name="adapt"/>
+            <label>Strongly Agree</label>
+        </div>
+        <div>
+            <h3>I am grateful that I took the time to learn PHP</h3>
+            <input type="radio" id="stronglyDisagree" name="grateful"/>
+            <label>Strongly Disagree</label>
+            <input type="radio" id="disagree"  name="grateful"/>
+            <label>Disagree</label>
+            <input type="radio" id="neutral" name="grateful"/>
+            <label>Neutral</label>
+            <input type="radio" id="agree" name="grateful"/>
+            <label>Agree</label>
+            <input type="radio" id="stronglyAgree" name="grateful"/>
+            <label>Strongly Agree</label>
+        </div>
 
     </body>
 </html>

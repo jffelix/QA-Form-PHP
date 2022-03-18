6585a719977fdb970d99e6516e4a5b7cd3f76fae
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-03-17 23:01:00 -0700

    Added comments

--- questions.php
+++ questions.php
@@ -15,56 +15,61 @@
         <p>Read the following statements and select the choice that applies the most to you</p>
         <br>
         <div>
-            <h3>PHP was a difficult language to learn</h3>
-            <input type="radio" id="stronglyDisagree" name="difficulty"/>
-            <label>Strongly Disagree</label>
-            <input type="radio" id="disagree"  name="difficulty"/>
-            <label>Disagree</label>
-            <input type="radio" id="neutral" name="difficulty"/>
-            <label>Neutral</label>
-            <input type="radio" id="agree" name="difficulty"/>
-            <label>Agree</label>
-            <input type="radio" id="stronglyAgree" name="difficulty"/>
-            <label>Strongly Agree</label>
-        </div>
-        <div>
-            <h3>I feel like I learned a lot from the experience</h3>
-            <input type="radio" id="stronglyDisagree" name="learning"/>
-            <label>Strongly Disagree</label>
-            <input type="radio" id="disagree"  name="learning"/>
-            <label>Disagree</label>
-            <input type="radio" id="neutral" name="learning"/>
-            <label>Neutral</label>
-            <input type="radio" id="agree" name="learning"/>
-            <label>Agree</label>
-            <input type="radio" id="stronglyAgree" name="learning"/>
-            <label>Strongly Agree</label>
-        </div>
-        <div>
-            <h3>I was able to adapt quickly to the language</h3>
-            <input type="radio" id="stronglyDisagree" name="adapt"/>
-            <label>Strongly Disagree</label>
-            <input type="radio" id="disagree"  name="adapt"/>
-            <label>Disagree</label>
-            <input type="radio" id="neutral" name="adapt"/>
-            <label>Neutral</label>
-            <input type="radio" id="agree" name="adapt"/>
-            <label>Agree</label>
-            <input type="radio" id="stronglyAgree" name="adapt"/>
-            <label>Strongly Agree</label>
-        </div>
-        <div>
-            <h3>I am grateful that I took the time to learn PHP</h3>
-            <input type="radio" id="stronglyDisagree" name="grateful"/>
-            <label>Strongly Disagree</label>
-            <input type="radio" id="disagree"  name="grateful"/>
-            <label>Disagree</label>
-            <input type="radio" id="neutral" name="grateful"/>
-            <label>Neutral</label>
-            <input type="radio" id="agree" name="grateful"/>
-            <label>Agree</label>
-            <input type="radio" id="stronglyAgree" name="grateful"/>
-            <label>Strongly Agree</label>
+            <form action="./completeForm.php" method="post">
+                <div>
+                    <h3>PHP was a difficult language to learn</h3>
+                    <input type="radio" id="stronglyDisagree" name="difficulty" value="stronglyDisagree"/>
+                    <label>Strongly Disagree</label>
+                    <input type="radio" id="disagree"  name="difficulty" value="disagree"/>
+                    <label>Disagree</label>
+                    <input type="radio" id="neutral" name="difficulty" value="neutral"/>
+                    <label>Neutral</label>
+                    <input type="radio" id="agree" name="difficulty" value="agree"/>
+                    <label>Agree</label>
+                    <input type="radio" id="stronglyAgree" name="difficulty" value="stronglyAgree"/>
+                    <label>Strongly Agree</label>
+                </div>
+                <div>
+                    <h3>I feel like I learned a lot from the experience</h3>
+                    <input type="radio" id="stronglyDisagree" name="learning" value="stronglyDisagree"/>
+                    <label>Strongly Disagree</label>
+                    <input type="radio" id="disagree"  name="learning" value="disagree"/>
+                    <label>Disagree</label>
+                    <input type="radio" id="neutral" name="learning" value="neutral"/>
+                    <label>Neutral</label>
+                    <input type="radio" id="agree" name="learning" value="agree"/>
+                    <label>Agree</label>
+                    <input type="radio" id="stronglyAgree" name="learning" value="stronglyAgree"/>
+                    <label>Strongly Agree</label>
+                </div>
+                <div>
+                    <h3>I was able to adapt quickly to the language</h3>
+                    <input type="radio" id="stronglyDisagree" name="adapt" value="stronglyDisagree"/>
+                    <label>Strongly Disagree</label>
+                    <input type="radio" id="disagree"  name="adapt" value="disagree"/>
+                    <label>Disagree</label>
+                    <input type="radio" id="neutral" name="adapt" value="neutral"/>
+                    <label>Neutral</label>
+                    <input type="radio" id="agree" name="adapt" value="agree"/>
+                    <label>Agree</label>
+                    <input type="radio" id="stronglyAgree" name="adapt" value="stronglyAgree"/>
+                    <label>Strongly Agree</label>
+                </div>
+                <div>
+                    <h3>I am grateful that I took the time to learn PHP</h3>
+                    <input type="radio" id="stronglyDisagree" name="grateful" value="stronglyDisagree"/>
+                    <label>Strongly Disagree</label>
+                    <input type="radio" id="disagree"  name="grateful" value="disagree"/>
+                    <label>Disagree</label>
+                    <input type="radio" id="neutral" name="grateful" value="neutral"/>
+                    <label>Neutral</label>
+                    <input type="radio" id="agree" name="grateful" value="agree"/>
+                    <label>Agree</label>
+                    <input type="radio" id="stronglyAgree" name="grateful" value="stronglyAgree"/>
+                    <label>Strongly Agree</label>
+                </div>
+                <input type="submit" name="submitAnswers"/>
+            </form>
         </div>
 
     </body>
--- verifyForm.php
+++ verifyForm.php
@@ -1,15 +1,19 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submitUser"])) {
 
+        // similar to mysql_connection in mysql library
         $connect = mysqli_connect('localhost', 'root', '', 'userInfo') or die('Connection failed: ' .mysqli_connect_error());
 
+        // checks if both input fields are not blank
         if ($_POST["name"] !== null && $_POST["email"] !== null) {
 
             $name = $_POST["name"];
             $email = $_POST["email"];
 
-            $sql = "INSERT INTO user (name, email) VALUES ('$name', '$email')";
+            // similar to queryStr
+            $sql = "INSERT INTO user (name, email, difficulty, learning, adapt, grateful) VALUES ('$name', '$email', null, null, null, null)";
 
+            // invokes the sql statement to the database
             $query = mysqli_query($connect, $sql);
 
             session_start();

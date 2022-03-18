e7a348e0b53bdb299126b21e5ea4281253dee47f
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-03-17 18:48:29 -0700

    Added files

---
+++ adminView.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin View</title>
+    </head>
+    <body>
+        <h1>Admin View</h1>
+    </body>
+</html>
---
+++ userForm.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>QA-Form (PHP)</title>
+    </head>
+    <body>
+        <h1>QA-Form (PHP)</h1>
+    </body>
+</html>

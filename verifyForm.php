a1799261f3b9211530f6c2e8c269b33acfb1179f
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-03-17 21:31:13 -0700

    verifyForm successfully submitted user info

--- verifyForm.php
+++ verifyForm.php
@@ -1,24 +1,16 @@
 <?php
-    // // isset is connected to action attribute from form
-    // if (isset($_POST["submit"])) {
-    //     $name = $_POST["name"];
-    //     $email = $_POST["email"];
-
-    //     Echo "Welcome " . $name;
-    // } 
-
     if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submitUser"])) {
 
-        $connect = mysqli_connect("localhost", "root", "", "userInfo") or die("Connection failed: " .mysqli_connect_error());
+        $connect = mysqli_connect('localhost', 'root', '', 'userInfo') or die('Connection failed: ' .mysqli_connect_error());
 
         if ($_POST["name"] !== null && $_POST["email"] !== null) {
 
             $name = $_POST["name"];
             $email = $_POST["email"];
 
-            $sql = "INSERT INTO `user` (`name`, `email`) VALUES ('$name', '$email')";
+            $sql = "INSERT INTO user (name, email) VALUES ('$name', '$email')";
 
-            $query = mysql_query($connect, $sql);
+            $query = mysqli_query($connect, $sql);
 
             if ($query) {
                 Echo "User form successfully submitted!";
